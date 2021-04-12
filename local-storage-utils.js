901813b17eee457aef4ce47d6d8d49255215353b
cmmerritt <christiane.merritt@gmail.com>
2021-04-12 14:23:27 -0700

    reformat links depending on quests completed

--- local-storage-utils.js
+++ local-storage-utils.js
@@ -75,3 +75,23 @@ export function updateUser(questId, choice) {
     user.dreams += choice.dreams;
     saveUser(user);
 }
+
+export function questIsCompleted(quest) {
+    const span = document.createElement('span');
+    span.classList.add('quest');
+    span.classList.add('completed');
+    span.style.top = quest.map.top;
+    span.style.left = quest.map.left;
+    span.textContent = quest.title;
+    return span;
+}
+
+export function createQuestLink(quest) {
+    const link = document.createElement('a');
+    link.classList.add('quest');
+    link.href = `../quest/?id=${quest.id}`;
+    link.style.top = quest.map.top;
+    link.style.left = quest.map.left;
+    link.textContent = quest.title;
+    return link;
+}
--- map/map.js
+++ map/map.js
@@ -1,22 +1,31 @@
 import quests from '../data-models.js';
-import { getUser, didUserExhaustQuests } from '../local-storage-utils.js';
+import { getUser, didUserExhaustQuests, questIsCompleted, createQuestLink } from '../local-storage-utils.js';
 
 const user = getUser();
-
 const userOutOfFP = user.friendshipPoints <= 0;
+const nav = document.getElementById('map-quests');
 
 if (didUserExhaustQuests() || userOutOfFP) {
     window.location = '../results/index.html';
 }
 
-const section = document.querySelector('section');
+// const section = document.querySelector('section');
 
-for (let quest of quests) {
+/* for (let quest of quests) {
     const anchorTag = document.createElement('a');
-    
     anchorTag.textContent = quest.title;
-    
     anchorTag.href = `../quest/?id=${quest.id}`;
-
     section.append(anchorTag);
+}
+ */
+
+for (let i = 0; i < quests.length; i++) {
+    const quest = quests[i];
+    let questDisplay = null;
+    if (user.completed[quest.id]) {
+        questDisplay = questIsCompleted(quest);
+    } else { 
+        questDisplay = createQuestLink(quest);
+    }
+    nav.appendChild(questDisplay);
 }
